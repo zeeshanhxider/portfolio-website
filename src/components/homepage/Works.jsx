b929cbbfc3420e63b3e66416ddf9fab09ebd6d2a
Zeeshan Haider <zeeshanhxider@gmail.com>
2025-08-30 17:52:15 +0500

    updated styling

--- src/components/homepage/Works.jsx
+++ src/components/homepage/Works.jsx
@@ -5,13 +5,22 @@ export default function Works({ forwardedRef }) {
     <section
       ref={forwardedRef}
       id="works"
-      className="nav-change overflow-hidden my-[20%]"
+      className="nav-change my-[20%] overflow-hidden"
     >
-     <Heading title="Projects" />
-      <div className="mt-10 grid grid-cols-1 gap-16 gap-y-10 md:grid-cols-12">
+      <Heading title="Projects" />
+      <div className="mt-10 grid grid-cols-1 gap-16 gap-y-10 md:grid-cols-12"></div>
+      <div className="flex w-full items-center justify-center space-x-20">
+        <h1 className="text-heading-1 font-medium leading-[1.25em] text-primary-200 md:leading-[1.08em]">
+          updating soon...
+        </h1>
       </div>
-       <div className="flex w-full items-center space-x-20 justify-center">
-        <h1 className="text-heading-1 font-medium text-primary-200 leading-[1.25em] md:leading-[1.08em]">
+      <div className="flex w-full items-center justify-center space-x-20 py-10">
+        <h1 className="font-outline-2 text-heading-1 font-medium leading-[1.25em] text-secondary-600 text-transparent md:leading-[1.08em]">
+          updating soon...
+        </h1>
+      </div>
+      <div className="flex w-full items-center justify-center space-x-20">
+        <h1 className="text-heading-1 font-medium leading-[1.25em] text-primary-200 md:leading-[1.08em]">
           updating soon...
         </h1>
       </div>
