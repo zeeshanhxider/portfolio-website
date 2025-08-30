e2398b550f6cfc40fb3843ef6ba7ef253909efe5
Zeeshan Haider <zeeshanhxider@gmail.com>
2025-08-30 16:11:49 +0500

    updated hero for mobile responsiveness

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -4,7 +4,8 @@ import { useRef, useEffect } from "react";
 export default function Hero() {
   const img = useRef(null);
   const imgContainer = useRef(null);
-  const titles = useRef([]);
+  const desktopTitles = useRef([]);
+  const mobileTitles = useRef([]);
   const scrollLine = useRef(null);
   const scroll = useRef(null);
 
@@ -30,7 +31,15 @@ export default function Hero() {
         { scale: 2, duration: 3.2, ease: "power4.inOut" },
         "-=3.1"
       )
-      .to(titles.current, { y: 0, duration: 2, ease: "power4.inOut" }, "-=2.5")
+      .to(
+        [...desktopTitles.current, ...mobileTitles.current],
+        {
+          y: 0,
+          duration: 2,
+          ease: "power4.inOut",
+        },
+        "-=2.5"
+      )
       .from(scroll.current, { opacity: 0, duration: 1, ease: "out" }, "-=2");
   }, []);
 
@@ -40,26 +49,72 @@ export default function Hero() {
       className="hero relative flex h-screen w-full select-none items-center justify-center"
       aria-label="hero"
     >
-      <div className="z-10 flex w-full  flex-col items-center text-title font-bold uppercase text-accent-300  2xl:space-y-16 2xl:text-[10vw]">
+      {/* MOBILE VERSION */}
+      <div className="z-10 flex w-full flex-col items-center text-title font-bold uppercase text-accent-300 sm:hidden -mt-10">
+        <div className="title py-2">
+          <h1
+            ref={(el) => (mobileTitles.current[0] = el)}
+            className="w-full translate-y-96 overflow-visible text-center text-[22.75vw] leading-none"
+          >
+            Hey, I&apos;m
+          </h1>
+          <h1
+            ref={(el) => (mobileTitles.current[1] = el)}
+            className="w-full translate-y-96 overflow-visible text-center text-[18vw] leading-none"
+          >
+            Zeeshan
+          </h1>
+        </div>
+        <div className="title py-2">
+          <h1
+            ref={(el) => (mobileTitles.current[2] = el)}
+            className="font-outline-3 w-full translate-y-96 overflow-visible text-center text-transparent text-[22.75vw] leading-none"
+          >
+            Hey, I&apos;m
+          </h1>
+          <h1
+            ref={(el) => (mobileTitles.current[3] = el)}
+            className="font-outline-3 w-full translate-y-96 overflow-visible text-center text-transparent text-[18vw] leading-none"
+          >
+            Zeeshan
+          </h1>
+        </div>
+        <div className="title py-2">
+          <h1
+            ref={(el) => (mobileTitles.current[4] = el)}
+            className="w-full translate-y-96 overflow-visible text-center text-[22.75vw] leading-none"
+          >
+            Hey, I&apos;m
+          </h1>
+          <h1
+            ref={(el) => (mobileTitles.current[5] = el)}
+            className="w-full translate-y-96 overflow-visible text-center text-[18vw] leading-none"
+          >
+            Zeeshan
+          </h1>
+        </div>
+      </div>
+      {/* DESKTOP VERSION */}
+      <div className="z-10 hidden w-full flex-col items-center text-title font-bold uppercase text-accent-300 sm:flex 2xl:space-y-16 2xl:text-[10vw]">
         <div className="title 2xl:py-16">
           <h1
-            ref={(el) => (titles.current[0] = el)}
+            ref={(el) => (desktopTitles.current[0] = el)}
             className="w-full translate-y-96 overflow-visible text-[11.3vw]"
           >
             Hey, I&apos;m zeeshan
           </h1>
         </div>
-        <div className=" title 2xl:py-16">
+        <div className="title 2xl:py-16">
           <h1
-            ref={(el) => (titles.current[1] = el)}
-            className="font-outline-3 md:font-outline-4 translate-y-96 overflow-visible text-transparent text-[11.3vw] w-full"
+            ref={(el) => (desktopTitles.current[1] = el)}
+            className="font-outline-3 md:font-outline-4 w-full translate-y-96 overflow-visible text-[11.3vw] text-transparent"
           >
             Hey, I&apos;m zeeshan
           </h1>
         </div>
-        <div className=" title 2xl:py-16">
+        <div className="title 2xl:py-16">
           <h1
-            ref={(el) => (titles.current[2] = el)}
+            ref={(el) => (desktopTitles.current[2] = el)}
             className="w-full translate-y-96 overflow-visible text-[11.3vw]"
           >
             Hey, I&apos;m zeeshan
@@ -68,7 +123,7 @@ export default function Hero() {
       </div>
       <div
         ref={imgContainer}
-        className="absolute mx-auto  w-[55%] overflow-hidden rounded-md"
+        className="absolute mx-auto w-[55%] overflow-hidden rounded-md"
       ></div>
     </section>
   );
